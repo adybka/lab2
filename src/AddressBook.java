495caf78f2ab435d30e40201a6f905dd8b00b585
andrewdybka <andrewdybka@mc6045-14.labs.sce.carleton.ca>
2018-09-14 09:13:40 -0400

    new thing

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,15 +2,27 @@ import java.util.ArrayList;
 
 public class AddressBook {
 	
-	static ArrayList<BuddyInfo> book = new ArrayList<BuddyInfo>();
+	private static ArrayList<BuddyInfo> book = new ArrayList<BuddyInfo>();
+	
+	public AddressBook() {
+		book = null;
+	}
 	
 
-	public static void addBuddy(BuddyInfo newAdd) {
+	public void addBuddy(BuddyInfo newAdd) {
 		book.add(newAdd);
 	}
 	
-	public static void removeBuddy() {
+	public void removeBuddy() {
 		book.remove(book.size()-1);
+	}
+	
+	public static void main(String[] args){
+		System.out.println("Address Book");
+		
+		BuddyInfo buddy = new BuddyInfo("Steve", 15, "123123");
+		AddressBook addBook = new AddressBook();
+		addBook.addBuddy(buddy);
 	}
 
 }
