c3b5c10cbe783807b362cb19a126bff5486d76d9
Andrew <Andrew@192.168.2.33>
2018-09-20 21:44:34 -0400

    added print?

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,19 +2,20 @@ import java.util.ArrayList;
 
 public class AddressBook {
 	
-	private static ArrayList<BuddyInfo> book = new ArrayList<BuddyInfo>();
+	private ArrayList<BuddyInfo> book;
 	
 	public AddressBook() {
-		book = null;
+		this.book = new ArrayList<BuddyInfo>();
 	}
 	
 
 	public void addBuddy(BuddyInfo newAdd) {
-		book.add(newAdd);
+		this.book.add(newAdd);
 	}
 	
 	public void removeBuddy() {
-		book.remove(book.size()-1);
+		this.book.remove(book.size()-1);
+		System.out.println("asdasd");
 	}
 	
 	public static void main(String[] args){
@@ -23,6 +24,7 @@ public class AddressBook {
 		BuddyInfo buddy = new BuddyInfo("Steve", 15, "123123");
 		AddressBook addBook = new AddressBook();
 		addBook.addBuddy(buddy);
+		addBook.removeBuddy();
 	}
 
 }
